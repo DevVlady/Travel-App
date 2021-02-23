7a6c62f56e4482fbc3defb9adc694e27f3b899a9
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-02-23 15:35:01 -0600

    Added the styling to the tags in the navbabar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import './Navbar.scss';
-import {BsHeart} from 'react-icons/bs';
-import {ImBell} from 'react-icons/im';
+import { BsFillHeartFill } from 'react-icons/bs';
+import { ImBell } from 'react-icons/im';
 
 const Navbar = () => {
 
     return (
         <div>
             <nav>
-                <a className='signinBtn' href="#">Sign-In</a>
-                <a href="#"><BsHeart /> Trips</a>
-                <a href="#"> <ImBell /> Notifications</a>
-                <h3>Travel App</h3>
+                <div>
+                    <a className='signinBtn' href="signin">Sign-In</a>
+                    <a href="/trips"><BsFillHeartFill /> Trips</a>
+                    <a href="/notifications"> <ImBell /> Notifications</a>
+                    <h3>Travel App</h3>
+                </div>
+                <div className='navOptions'>
+                    <a href="/flights">Flights</a>
+                    <a href="/flights">Flights</a>
+                    <a href="/flights">Flights</a>
+                    <a href="/flights">Flights</a>
+                    <a href="/flights">Flights</a>
+                    <a href="/flights">Flights</a>
+                </div>
             </nav>
         </div>
     )
