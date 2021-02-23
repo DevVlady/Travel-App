631eef3582f1d173f794df35989f79c27f65991c
S Galarza <sgalarza4190@gmail.com>
2021-02-23 18:55:02 -0500

    added card component

---
+++ src/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Card = () => {
+    return (
+        <div className="card">
+            <div className="card-body">
+                <h5 className="card-title">card title</h5>
+                <h6 className="card-subtitle mb-2 text-muted">card subtitle</h6>
+                <p className="card-text">paragraph text</p>
+                <a href="#" className="card-link">Card Link</a>
+                <a href="#" className="card-link">another link</a>
+            </div>
+        </div>
+    )
+}
+
+export default Card
