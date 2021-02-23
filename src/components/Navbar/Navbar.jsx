dc410243aada0e6dbab59e194330468903567d14
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-02-23 14:44:31 -0600

    Added the sign in button at the top of the page inside the navbar.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import './Navbar.sass';
+import './Navbar.scss';
 
 const Navbar = () => {
 
     return (
         <div>
+            <nav>
+                <a href="#">Sign-In</a>
+            </nav>
         </div>
     )
 }
