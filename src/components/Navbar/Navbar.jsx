8c62a3b58a398782e4481a683eb392b067f40beb
Vladimir <vladimir.s.dimitrov14@gmail.com>
2021-02-23 15:03:11 -0600

    Styled the navbar and added the links along with the title of the application.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './Navbar.scss';
+import {BsHeart} from 'react-icons/bs';
+import {ImBell} from 'react-icons/im';
 
 const Navbar = () => {
 
     return (
         <div>
             <nav>
-                <a href="#">Sign-In</a>
+                <a className='signinBtn' href="#">Sign-In</a>
+                <a href="#"><BsHeart /> Trips</a>
+                <a href="#"> <ImBell /> Notifications</a>
+                <h3>Travel App</h3>
             </nav>
         </div>
     )
