730738e94d81c360b782542c55f6b494c5f3959d
S Galarza <sgalarza4190@gmail.com>
2021-02-24 17:43:27 -0500

    edited cards

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,69 +7,73 @@ import {MdRestaurant, MdRateReview, MdLocalHotel} from 'react-icons/md';
 
 const Header = () => {
 
+    const styled = {
+    }
+
     return (
         <div className="header">
             <div className="container">
                 <div className="row">
-                    <div className="col-lg-2 col-md-6 col-xs-6 card-container">
+                    <div className="col-lg-4 col-md-6 col-xs-12 card-container">
                         <a href="/flights">
                             <div className="card">
                                 <div className="card-body">
-                                    <h5 className="card-title">card title</h5>
+                                    <h3 className="card-title">Flights <GiCommercialAirplane style={styled}/></h3>
                                 </div>
                             </div>
                         </a>
                     </div>
                     {/*  */}
-                    <div className="col-lg-2 col-md-6 col-xs-6 card-container">
-                        <a href="/flights">
+                    <div className="col-lg-4 col-md-6 col-xs-12 card-container">
+                        <a href="/hotels">
                             <div className="card">
                                 <div className="card-body">
-                                    <h5 className="card-title">card title</h5>
+                                    <h3 className="card-title">Hotels <MdLocalHotel style={styled}/></h3>
                                 </div>
                             </div>
                         </a>
                     </div>
                     {/*  */}
-                    <div className="col-lg-2 col-md-6 col-xs-6 card-container">
-                        <a href="/flights">
+                    <div className="col-lg-4 col-md-6 col-xs-12 card-container">
+                        <a href="/todo">
                             <div className="card">
                                 <div className="card-body">
-                                    <h5 className="card-title">card title</h5>
+                                    <h3 className="card-title">To Do <RiFileList3Fill style={styled}/></h3>
                                 </div>
                             </div>
                         </a>
                     </div>
                     {/*  */}
-                    <div className="col-lg-2 col-md-6 col-xs-6 card-container">
-                        <a href="/flights">
+                    <div className="col-lg-4 col-md-6 col-xs-12 card-container">
+                        <a href="/restaurants">
                             <div className="card">
                                 <div className="card-body">
-                                    <h5 className="card-title">card title</h5>
+                                    <h3 className="card-title">Restaurants <MdRestaurant style={styled}/></h3>
                                 </div>
                             </div>
                         </a>
                     </div>
                     {/*  */}
-                    <div className="col-lg-2 col-md-6 col-xs-6 card-container">
-                        <a href="/flights">
+                    <div className="col-lg-4 col-md-6 col-xs-12 card-container">
+                        <a href="/reviews">
                             <div className="card">
                                 <div className="card-body">
-                                    <h5 className="card-title">card title</h5>
+                                    <h3 className="card-title">Reviews <MdRateReview style={styled}/></h3>
                                 </div>
                             </div>
                         </a>
                     </div>
                     {/*  */}
-                    <div className="col-lg-2 col-md-6 col-xs-6 card-container">
-                        <a href="/flights">
+                    <div className="col-lg-4 col-md-6 col-xs-12 card-container">
+                        <a href="/more">
                             <div className="card">
                                 <div className="card-body">
-                                    <h5 className="card-title">card title</h5>
+                                    <h3 className="card-title">More <BsThreeDots style={styled}/></h3>
                                 </div>
                             </div>
                         </a>
                     </div>
+                    {/*  */}
                 </div>
             </div>
         </div>
