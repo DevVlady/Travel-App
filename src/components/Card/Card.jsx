4243cb95159e2bf05cbad8088c4b2b8f2a5019f3
S Galarza <sgalarza4190@gmail.com>
2021-02-24 17:05:22 -0500

    added links to other pages on cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import './Card.scss';
+import { BsThreeDots } from 'react-icons/bs';
+import {GiCommercialAirplane} from 'react-icons/gi';
+import {RiFileList3Fill} from 'react-icons/ri';
+import {MdRestaurant, MdRateReview, MdLocalHotel} from 'react-icons/md';
 
 const Card = () => {
-    
+
+    const styled = {
+        float: 'right'
+    }    
 
     return (
         <div className="card">
@@ -10,7 +17,7 @@ const Card = () => {
                 <h5 className="card-title">card title</h5>
                 {/* <h6 className="card-subtitle mb-2 text-muted">card subtitle</h6> */}
                 {/* <p className="card-text">paragraph text</p> */}
-                <a href="#" className="card-link">ICON</a>
+                {/* <a href="#" className="card-link">ICON</a> */}
                 {/* <a href="#" className="card-link">another link</a> */}
             </div>
         </div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,29 +1,46 @@
 import React from 'react';
 import './Header.scss';
 import Card from '../Card/Card';
+import { BsThreeDots } from 'react-icons/bs';
+import {GiCommercialAirplane} from 'react-icons/gi';
+import {RiFileList3Fill} from 'react-icons/ri';
+import {MdRestaurant, MdRateReview, MdLocalHotel} from 'react-icons/md';
 
 const Header = () => {
+
     return (
         <div className="header">
             <div className="container">
                 <div className="row">
                     <div className="col-lg-2 col-md-6 col-xs-6 card-container">
+                    <a href="/flights">
                     <Card />
+                    </a>
                     </div>
                     <div className="col-lg-2 col-md-6 col-xs-6 card-container">
+                    <a href="/hotels">
                     <Card />
+                    </a>
                     </div>
                     <div className="col-lg-2 col-md-6 col-xs-6 card-container">
+                    <a href="/todo">
                     <Card />
+                    </a>
                     </div>
                     <div className="col-lg-2 col-md-6 col-xs-6 card-container">
+                    <a href="/restaurants">
                     <Card />
+                    </a>
                     </div>
                     <div className="col-lg-2 col-md-6 col-xs-6 card-container">
+                    <a href="/reviews">
                     <Card />
+                    </a>
                     </div>
                     <div className="col-lg-2 col-md-6 col-xs-6 card-container">
+                    <a href="/more">
                     <Card />
+                    </a>
                     </div>
                 </div>
             </div>
